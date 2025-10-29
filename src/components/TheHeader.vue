befb6c8ef724f3142f0ca31846694c115369f629
LucasMejer <lucas.mm@live.dk>
2025-10-29 12:15:32 +0100

    start på header

---
+++ src/components/TheHeader.vue
@@ -0,0 +1,16 @@
+<script setup>
+</script>
+
+<template>
+    <header>
+      Test
+      <ul>
+        <li>
+          Forside
+        </li>
+        <li>
+          Om os
+        </li>
+      </ul>
+    </header>
+</template>
