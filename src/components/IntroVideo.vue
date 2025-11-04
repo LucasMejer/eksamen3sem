1af42436610078187be33cedf2f006dcd610ce05
LucasMejer <lucas.mm@live.dk>
2025-11-04 13:55:05 +0100

    IntroVideo

---
+++ src/components/IntroVideo.vue
@@ -0,0 +1,9 @@
+<script setup>
+</script>
+
+
+<template>
+  <div class="introVideoDiv">
+    <img src="../../public/introvideotemp.png">
+  </div>
+</template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,11 @@
 <script setup>
+import IntroVideo from '@/components/IntroVideo.vue';
 import TheHeader from '@/components/TheHeader.vue';
 </script>
 
 <template>
   <main>
     <the-header />
+    <IntroVideo />
   </main>
 </template>
